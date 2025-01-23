ac08cc6710c9de215ad34e9656e5cddd03077426
Farhad Islam Hridoy <farhadislamhridoy@gmail.com>
2025-01-24 01:48:31 +0600

    Create DeleteTheLastNode.cpp

---
+++ Linked List/DeleteTheLastNode.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+typedef struct node{
+    int data;
+    struct node * next;
+}node;
+
+node *head, *temp, *newnode;
+void DeleteFromLast(node **head){
+        node * prev ;
+            if(*head == NULL){
+                cout << "List is empty" << endl;
+                return;
+            } else if ((*head)->next==NULL) {
+                    delete *head;
+                    *head = NULL;
+            } else {
+                temp = *head;
+                while(temp->next!=NULL){
+                    prev = temp;
+                    temp = temp->next;
+                }
+                prev->next = NULL;
+                delete temp;
+            }
+
+
+}
+
+void DisplayNodes(node **head){
+    temp = *head;
+        while(temp!=NULL){
+            cout << temp->data << " ";
+            temp = temp ->next;
+        }
+            cout << "\n";
+}
+void DeletingNodes(node **head){
+    while(*head!=NULL){
+        temp = *head;
+        *head = (*head)->next;
+        delete temp;
+    }
+
+}
+void NodeSets(){
+    head = NULL;
+    int add = 1;
+    while(add){
+        newnode = new node;
+            if(newnode == NULL){
+                cout << "No memory is allocated" <<endl;
+                return;
+            }
+                cout << "Enter Data to the node: ";
+                cin  >> newnode->data;
+                newnode->next = NULL;
+
+                    if(head==NULL){
+                        head = temp = newnode;
+                    } else {
+                        temp->next = newnode;
+                        temp = newnode;
+                    }
+
+                cout << "Press (1 or 0): ";
+                cin  >> add;
+    }
+    DisplayNodes(&head);
+    DeleteFromLast(&head);
+    DisplayNodes(&head);
+    DeletingNodes(&head);
+
+}
+
+int main(){
+    NodeSets();
+
+    return 0;
+}
+
